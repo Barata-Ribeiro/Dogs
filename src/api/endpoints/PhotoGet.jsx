e4aad49f1b194b8bf087087290446f9f052d11d1
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-10 14:14:19 -0300

    Teste da API Dogs

---
+++ src/api/endpoints/PhotoGet.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const PhotoGet = () => {
+  const [id, setId] = React.useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`https://dogsapi.origamid.dev/json/api/photo/${id}`)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        return data;
+      });''
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={id}
+        onChange={({ target }) => setId(target.value)}
+      />
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default PhotoGet;
