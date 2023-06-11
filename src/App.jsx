fe6cf673a8ec64921e2bfd66a37bf6e6023db959
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-11 10:07:53 -0300

    Add react router dom ao App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Home from "./Components/Home";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 };
 
