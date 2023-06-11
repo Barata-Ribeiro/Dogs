f3f030092484f05699c35c3f7062fc7c4b34bb78
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-11 10:21:09 -0300

    Add CSS reset e rotas iniciais

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./styles/Header.module.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      <nav className="container">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Criar</Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
