0240aac5702696b29d61293f3bfccd7672a50d0a
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-12 09:22:42 -0300

    Add custom hook useForm para validação no login

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from "../styles/Forms/Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label className={styles.label} htmlFor={name}>
         {label}
       </label>
-      <input className={styles.input} type={type} name={name} id={name} />
-      <p className={styles.error}>Error</p>
+      <input
+        className={styles.input}
+        type={type}
+        name={name}
+        id={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
