015c1bc4e92d45a704ecc0dbb485dd0362571d73
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-12 09:00:51 -0300

    Add estilos do componente input

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -3,12 +3,13 @@ import styles from "../styles/Forms/Input.module.css";
 
 const Input = ({ label, type, name }) => {
   return (
-    <>
-      <label htmlFor={name}>
+    <div className={styles.wrapper}>
+      <label className={styles.label} htmlFor={name}>
         {label}
-        <input className={styles.input} type={type} name={name} id={name} />
       </label>
-    </>
+      <input className={styles.input} type={type} name={name} id={name} />
+      <p className={styles.error}>Error</p>
+    </div>
   );
 };
 
