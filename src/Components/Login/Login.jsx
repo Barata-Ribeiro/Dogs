c2ff13096d1fc0d5c7880a2740e01b669614895e
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-15 07:03:27 -0300

    add imagem e estilos gerais de login forms

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,20 +5,23 @@ import LoginCreate from "./LoginCreate";
 import LoginLostPassword from "./LoginLostPassword";
 import LoginResetPassword from "./LoginResetPassword";
 import { UserContext } from "../../UserContext";
+import styles from "../styles/Login.module.css";
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="recuperar" element={<LoginLostPassword />} />
-        <Route path="resetar" element={<LoginResetPassword />} />
-      </Routes>
-    </>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="recuperar" element={<LoginLostPassword />} />
+          <Route path="resetar" element={<LoginResetPassword />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
