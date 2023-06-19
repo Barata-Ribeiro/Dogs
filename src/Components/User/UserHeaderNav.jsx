3c893e3123c7cb1f6c92d05185c10ac20657ae4e
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-19 07:21:44 -0300

    add menu hamburger dashboard

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -6,11 +6,13 @@ import { ReactComponent as Estatisticas } from "../../assets/estatisticas.svg";
 import { ReactComponent as AdicionarFoto } from "../../assets/adicionar.svg";
 import { ReactComponent as Sair } from "../../assets/sair.svg";
 import styles from "../styles/UserHeaderNav.module.css";
+import useMedia from "../../Hooks/useMedia";
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
+  const mobile = useMedia("(max-width: 40rem)");
   const { userLogout } = React.useContext(UserContext);
   const navigate = useNavigate();
+  const [mobileMenu, setMobileMenu] = React.useState(false);
 
   const handleLogout = () => {
     userLogout();
@@ -18,24 +20,33 @@ const UserHeaderNav = () => {
   };
 
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/conta" end>
-        <MinhasFotos />
-        {mobile && "Minhas Fotos"}
-      </NavLink>
-      <NavLink to="/conta/estatisticas">
-        <Estatisticas />
-        {mobile && "Estatísticas"}
-      </NavLink>
-      <NavLink to="/conta/postar">
-        <AdicionarFoto />
-        {mobile && "Adicionar Foto"}
-      </NavLink>
-      <button onClick={handleLogout}>
-        <Sair />
-        {mobile && "Sair"}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          className={styles.mobileButton}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+      <nav className={styles.nav}>
+        <NavLink to="/conta" end>
+          <MinhasFotos />
+          {mobile && "Minhas Fotos"}
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Estatisticas />
+          {mobile && "Estatísticas"}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <AdicionarFoto />
+          {mobile && "Adicionar Foto"}
+        </NavLink>
+        <button onClick={handleLogout}>
+          <Sair />
+          {mobile && "Sair"}
+        </button>
+      </nav>
+    </>
   );
 };
 
