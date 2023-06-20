3bef22ac26e5b85629c507811db8b1bc6cbce782
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-20 05:59:24 -0300

    estrutura inicial do user photo post

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import styles from "../styles/UserPhotoPost.module.css";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../hooks/useForm";
+import useFetch from "../../hooks/useFetch";
 
 const UserPhotoPost = () => {
-  return <div>Postar foto</div>;
+  const nome = useForm();
+  const peso = useForm("number");
+  const idade = useForm("number");
+  const [img, setImg] = React.useState({});
+  const { data, error, loading, request } = useFetch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("img", img.raw);
+    formData.append("nome", nome.value);
+    formData.append("peso", peso.value);
+    formData.append("idade", idade.value);
+
+    request(url, options);
+  };
+
+  const handleImgChange = ({ target }) => {
+    setImg({
+      raw: target.files[0],
+    });
+  };
+
+  return (
+    <section className={`${styles.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="nome" />
+        <Input label="Peso" type="text" name="peso" />
+        <Input label="Idade" type="text" name="idade" />
+        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <Button>Enviar</Button>
+      </form>
+    </section>
+  );
 };
 
 export default UserPhotoPost;
