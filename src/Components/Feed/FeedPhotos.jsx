6a95bdc8022573ba5c656bf2a6e45c7a122c7857
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-22 08:49:47 -0300

    add instrução de estilos, add fotos do feed

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -4,6 +4,7 @@ import useFetch from "../../Hooks/useFetch";
 import { PHOTOS_GET } from "../../Api";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
+import styles from "../styles/FeedPhotos.module.css";
 
 const FeedPhotos = () => {
   const { data, loading, error, request } = useFetch();
@@ -19,11 +20,15 @@ const FeedPhotos = () => {
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
-  return (
-    <div>
-      <FeedPhotosItem />
-    </div>
-  );
+  if (data) {
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} />
+        ))}
+      </ul>
+    );
+  } else return null;
 };
 
 export default FeedPhotos;
