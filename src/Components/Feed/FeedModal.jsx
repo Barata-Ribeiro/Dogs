3872d5a3cd6107d6c769b1f36d077d2227b567e6
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-23 06:35:57 -0300

    add logica inicial do feed modal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/FeedModal.module.css";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTO_GET } from "../../Api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
 
-const FeedModal = () => {
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
   return (
-    <div>FeedModal</div>
-  )
-}
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <img src={data.src} alt="" />}
+    </div>
+  );
+};
 
-export default FeedModal
+export default FeedModal;
