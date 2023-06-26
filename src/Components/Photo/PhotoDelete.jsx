037118b99a1dd459710cf4d8da1d3291f05699dc
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-26 06:12:41 -0300

    add lógica de deletar foto

--- src/Components/Photo/PhotoDelete.jsx
+++ src/Components/Photo/PhotoDelete.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import styles from "../styles/PhotoDelete.module.css";
+import { PHOTO_DELETE } from "../../Api";
+import useFetch from "../../Hooks/useFetch";
 
 const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const { url, options } = PHOTO_DELETE(id);
+    const { response } = await request(url, options);
+    if (response.ok) window.location.reload();
+  };
+
   return (
     <>
-      <button className={styles.delete}>Deletar</button>
+      <button onClick={handleClick} className={styles.delete}>
+        Deletar
+      </button>
     </>
   );
 };
