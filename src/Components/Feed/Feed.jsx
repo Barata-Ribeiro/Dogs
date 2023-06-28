54dc290c1811acfb337bcda334cd5e71c2fde953
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-28 06:21:45 -0300

    add user photos em 'minhas fotos'

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
 
-const Feed = () => {
+const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
 
   return (
     <div>
-      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />}
-      <FeedPhotos setModalPhoto={setModalPhoto} />
+      {modalPhoto && (
+        <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
+      )}
+      <FeedPhotos user={user} setModalPhoto={setModalPhoto} />
     </div>
   );
 };
