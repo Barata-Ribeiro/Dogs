28b87d0adba453a18525aa8be1a61cb6cf8bf42a
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-30 07:59:40 -0300

    add estrutura inicial do reset do password

--- src/Components/Login/LoginResetPassword.jsx
+++ src/Components/Login/LoginResetPassword.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginResetPassword = () => {
-  return (
-    <div>LoginResetPassword</div>
-  )
-}
+  const [login, setLogin] = React.useState("");
+  const [key, setKey] = React.useState("");
+  const password = useForm("password");
 
-export default LoginResetPassword
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get("key");
+    const login = params.get("login");
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <p>{key}</p>
+      <p>{login}</p>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nova Senha" type="password" name="passowrd" />
+        <Button>Resetar</Button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginResetPassword;
