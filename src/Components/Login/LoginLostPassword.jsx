9bd8243901ac0c4886b2970f476934817d6b2570
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-06-30 07:37:00 -0300

    add estrutura de 'perdeu a senha'

--- src/Components/Login/LoginLostPassword.jsx
+++ src/Components/Login/LoginLostPassword.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
 
 const LoginLostPassword = () => {
-  return (
-    <div>LoginLostPassword</div>
-  )
-}
+  const email = useForm("email");
+  const { data, loading, error, request } = useFetch();
 
-export default LoginLostPassword
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Email / Usuário" type="text" name="email" {...email} />
+        <Button>Enviar Email</Button>
+      </form>
+    </section>
+  );
+};
+
+export default LoginLostPassword;
