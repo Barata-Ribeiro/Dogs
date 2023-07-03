04727e10d3b0771afea2ae365990cc33d884f4ce
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-03 06:26:47 -0300

    add footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "./styles/Footer.module.css";
+import { ReactComponent as Dogs } from "../assets/dogs-footer.svg";
 
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>2020 - {currentYear} &copy; Dogs. Alguns direitos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
