fc2ff70e00a801f18f9b2ef035deca84d1bdcee3
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-07-04 06:28:35 -0300

    add estrutura inicial de estatísticas

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import Head from "../Helper/Head";
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../Api";
+import Loading from "../Helper/Loading";
+import Error from "../Helper/Error";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head
-        title="Estatísticas"
-        description="Estatísticas sobre as postagens do usuário."
-      />
-      Estatisticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    };
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head
+          title="Estatísticas"
+          description="Estatísticas sobre as postagens do usuário."
+        />
+        Estatisticas
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
