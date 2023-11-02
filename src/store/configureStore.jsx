e2db98a859d539b3491b0caf5b62e4a98440d3cc
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-02 09:14:26 -0300

    feat: add 'photoPost' reducer

--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,12 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import feed from "./feed";
 import photo from "./photo";
+import photoPost from "./photoPost";
 import token from "./token";
 import ui from "./ui";
 import user from "./user";
 
 const middleware = (getDefaultMiddleware) => [...getDefaultMiddleware()];
-const reducer = combineReducers({ photo, token, user, feed, ui });
+const reducer = combineReducers({ photo, photoPost, token, user, feed, ui });
 const store = configureStore({ reducer, middleware });
 
 export default store;
