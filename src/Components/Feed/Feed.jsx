45453d26ecd5a42e8a67be0317fd5d1bb462d7ff
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-02 08:44:22 -0300

    perf: fix 'react' imports, implement 'feed' reducer to 'Feed'

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import PropTypes from "prop-types";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadNewPhotos, resetFeedState } from "../../store/feed";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
 import FeedModal from "./FeedModal";
 import FeedPhotos from "./FeedPhotos";
-import PropTypes from "prop-types";
 
 const Feed = ({ user }) => {
-  const [modalPhoto, setModalPhoto] = React.useState(null);
-  const [pages, setPages] = React.useState([1, 2]);
-  const [infinite, setInfinite] = React.useState(true);
+  const [modalPhoto, setModalPhoto] = useState(null);
+  const { infinite, loading, list, error } = useSelector((state) => state.feed);
+  const dispatch = useDispatch();
+  const wait = useRef(false);
 
-  React.useEffect(() => {
-    let wait = false;
+  useEffect(() => {
+    dispatch(resetFeedState());
+    dispatch(loadNewPhotos({ user, total: 6 }));
+  }, [dispatch, user]);
+
+  useEffect(() => {
     const infiniteScroll = () => {
       if (infinite) {
         const scroll = window.scrollY;
         const height = document.body.offsetHeight - window.innerHeight;
-        if (scroll > height * 0.75 && !wait) {
-          setPages((pages) => [...pages, pages.length + 1]);
-          wait = true;
+        if (scroll > height * 0.75 && !wait.current) {
+          dispatch(loadNewPhotos({ user, total: 6 }));
+          wait.current = true;
           setTimeout(() => {
-            wait = false;
+            wait.current = false;
           }, 500);
         }
       }
@@ -30,22 +39,27 @@ const Feed = ({ user }) => {
       window.removeEventListener("wheel", infiniteScroll);
       window.removeEventListener("scroll", infiniteScroll);
     };
-  }, [infinite]);
+  }, [infinite, dispatch, user]);
 
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
   return (
     <div>
       {modalPhoto && (
         <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
       )}
-      {pages.map((page) => (
-        <FeedPhotos
-          key={page}
-          user={user}
-          page={page}
-          setModalPhoto={setModalPhoto}
-          setInfinite={setInfinite}
-        />
-      ))}
+      {list.length > 0 && <FeedPhotos setModalPhoto={setModalPhoto} />}
+      {!infinite && !user && (
+        <p
+          style={{
+            textAlign: "center",
+            padding: "2rem 0 4rem 0",
+            color: "#888",
+          }}
+        >
+          Não existem mais postagens.
+        </p>
+      )}
     </div>
   );
 };
